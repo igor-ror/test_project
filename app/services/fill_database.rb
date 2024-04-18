c4c5ce5bde3980db84eef9061363c7bcc7e6c2af
Igor Babenko <igor.b@railsmuffin.com>
2024-04-18 18:18:32 +0300

    Fix FillDatabase service to use insert all

--- app/services/fill_database.rb
+++ app/services/fill_database.rb
@@ -16,23 +16,32 @@ class FillDatabase
   ]
 
   def call
-    ages = (1..50).to_a
-    weights = (1..100).to_a
-    deeps = (1..1000).to_a
-    fish_counts = (1..10000).to_a
+    ages          = (1..50).to_a
+    weights       = (1..100).to_a
+    deeps         = (1..1000).to_a
+    fish_counts   = (1..10000).to_a
+    habitats_data = []
+    fish_data     = []
 
     RECORDS_COUNT.times do |num|
-      fish_name = FISH_NAMES.sample
       habitat_name = HABITAT_NAMES.sample
-      age = ages.sample
-      weigth = weights.sample
       deep = deeps.sample
       fish_count = fish_counts.sample
 
-      habitat = Habitat.create!(name: habitat_name, max_deep: deep, fish_count: fish_count)
-      fish = Fish.create!(name: fish_name, weight: weigth, age: age, area: habitat_name, habitat_id: habitat.id)
-
-      puts "#{num} fish created"
+      habitats_data << { name: habitat_name, max_deep: deep, fish_count: fish_count }
     end
+
+    Habitat.insert_all(habitats_data)
+    puts "Habitats created"
+
+    Habitat.find_each do |habitat|
+      age = ages.sample
+      fish_name = FISH_NAMES.sample
+      weigth = weights.sample
+
+      fish_data << { name: fish_name, weight: weigth, age: age, area: habitat.name, habitat_id: habitat.id }
+    end
+
+    Fish.insert_all(fish_data)
   end
 end
